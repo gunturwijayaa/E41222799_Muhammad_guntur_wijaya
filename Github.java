3dbc0060a32b1a28abc8dc5764922acc528187bd
gunturwijayaa <124749904+gunturwijayaa@users.noreply.github.com>
2023-02-12 19:31:10 +0700

    Create Github.java

---
+++ Github.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github;
+
+/**
+ *
+ * @author guntu
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("muhammad guntur wijaya");
+        System.out.print("golongan E");
+        System.out.print("nim : E41222799");
+    }
+    
+}
